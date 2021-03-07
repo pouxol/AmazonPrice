6caa403169170778dbe60d50abbc4861e144c288
pouxol <41949525+pouxol@users.noreply.github.com>
2021-03-07 14:01:46 +0100

    minor

--- main.py
+++ main.py
@@ -5,7 +5,8 @@ import lxml
 url = "https://www.amazon.de/-/en/dp/B07YCRQBB9/"
 
 headers = {
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.190 Safari/537.36",
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) "
+                  "Chrome/88.0.4324.190 Safari/537.36",
     "Accept-Language": "de-DE,de;q=0.9,en;q=0.8,en-US;q=0.7"
 }
 
